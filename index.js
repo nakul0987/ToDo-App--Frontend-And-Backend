2f64e0cdd3ce8e798da36b11219c59835f0259f5
nakul sharma <nakulmac12gmail.com@Nakuls-MacBook-Air.local>
2025-11-23 21:45:59 +0530

    ToDo APP

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
+
+let todos=[];
+
+app.get("/", (req, res) => {
+    res.render('home.ejs',{todos})
+});
+
+app.post('/todos',(req,res)=>{
+    todos.push(req.body);
+    res.redirect('/');
+});
+
+app.delete('/todos/:id',(req,res)=>{
+    let id=Number(req.params.id);
+    todos.splice(id,1);
+    res.redirect('/');
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
+
